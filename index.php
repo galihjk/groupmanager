11c1562878fab697ab39b2cf1a893af519bf478f
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2022-12-05 13:30:06 +0700

    makedir

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php echo "hello world"; //--tes
+
 $folder_data = "/data/tes/";
 if(!file_exists($folder_data)){
-    mkdir($folder_data, 0777);
+    mkdir($folder_data, 0777, true);
     echo "DIBUAT: [$folder_data] ";
 }
 $folder = "/data/tes/nama/";
@@ -9,6 +10,6 @@ if(file_exists($folder)){
     print_r(scandir($folder));
 }
 else{
-    mkdir($folder, 0777);
+    mkdir($folder, 0777, true);
     echo "DIBUAT";
 }
