e13d8d50f5f897b7d5204587d21c7b26e1e58eda
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2022-12-05 13:32:15 +0700

    ok

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
 <?php echo "hello world"; //--tes
 
-$folder_data = "/data/tes/";
-if(!file_exists($folder_data)){
-    mkdir($folder_data, 0777, true);
-    echo "DIBUAT: [$folder_data] ";
-}
 $folder = "/data/tes/nama/";
 if(file_exists($folder)){
     print_r(scandir($folder));
 }
 else{
-    mkdir($folder, 0777, true);
-    echo "DIBUAT";
+    echo "ERROR: File does not exist [$folder]";
 }
