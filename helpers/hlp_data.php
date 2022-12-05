be4e56437d6dab0d988d97f7d21f41fbd244832a
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2022-12-05 14:02:53 +0700

    data_insert

--- helpers/hlp_data.php
+++ helpers/hlp_data.php
@@ -1,11 +1,27 @@
 <?php
-function is_dir_empty($dir) {
-    if (!is_readable($dir)) return null; 
-    return (count(scandir($dir)) == 2);
-}
 
-function data_insert ($table, $data){
-    // $cols = array_keys($data);
-    // $primary_key = 
-    // return "asd";
+function data_insert($table, $data){
+    $cols = array_keys($data);
+    foreach($cols as $col){
+        $folder = "data/$table/$col/";
+        if(!file_exists($folder)){
+            mkdir($folder, 0777, true);
+        }
+    }
+    $id = 1;
+    if(is_readable($folder)){
+        $scandir = scandir($folder);
+        sort($scandir);
+        $scandircount = count($scandir);
+        if($scandircount > 2){
+            $id = $scandir[$scandircount-1] + 1;
+        }
+        else{
+            $id = 1;
+        }
+    }
+    foreach($data as $col=>$val){
+        file_put_contents("data/$table/$col/$id",$val);
+    }
+    return $id;
 }
--- index.php
+++ index.php
@@ -4,27 +4,36 @@ $folder = "data/tes/nama/";
 
 include_once("helpers/hlp_data.php");
 
-if(file_exists($folder)){
-    if(!is_readable($folder)){
-        $id = 1;
-        echo "not readable";
-    }
-    else{
-        $scandir = scandir($folder);
-        sort($scandir);
-        $scandircount = count($scandir);
-        echo "[scandircount$scandircount]<pre>";
-        print_r($scandir);
-        if($scandircount > 2){
-            $id = $scandir[$scandircount-1] + 1;
+if(!empty($_GET['tesinsert'])){
+    echo data_insert("tezt",[
+        'name' => "name" . $_GET['tesinsert'],
+        'address' => "address" . $_GET['tesinsert'],
+    ]);
+}
+
+/*
+    if(file_exists($folder)){
+        if(!is_readable($folder)){
+            $id = 1;
+            echo "not readable";
         }
         else{
-            $id = 1;
+            $scandir = scandir($folder);
+            sort($scandir);
+            $scandircount = count($scandir);
+            echo "[scandircount$scandircount]<pre>";
+            print_r($scandir);
+            if($scandircount > 2){
+                $id = $scandir[$scandircount-1] + 1;
+            }
+            else{
+                $id = 1;
+            }
         }
+        echo "NEW ID=$id";
     }
-    echo "NEW ID=$id";
-}
-else{
-    mkdir($folder, 0777, true);
-    echo "DIBUAT";
-}
+    else{
+        mkdir($folder, 0777, true);
+        echo "DIBUAT";
+    }
+*/
