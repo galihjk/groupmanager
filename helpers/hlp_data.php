76fd457546f3220cf8471c9ef920aefc69552b0c
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2022-12-06 06:00:38 +0700

    data_get_one

--- helpers/hlp_data.php
+++ helpers/hlp_data.php
@@ -1,5 +1,7 @@
 <?php
 
+$data_global = [];
+
 function data_insert($table, $data_insert){
     $folder = "";
     $checkcols = array_keys($data_insert);
@@ -20,4 +22,56 @@ function data_insert($table, $data_insert){
     }
     file_put_contents("data/$table/last.id",$id);
     return $id;
+}
+
+function data_get_one($table, $id, $cols = [], $refresh = false){
+    global $data_global;
+    if(!$refresh){
+        if(empty($data_global[$table][$id])){
+            $refresh = true;
+        }
+    }
+    if(!$refresh){
+        if(!empty($cols)){
+            foreach($cols as $col){
+                if(empty($data_global[$table][$id][$col])){
+                    $refresh = true;
+                    break;
+                }
+            }
+        }
+    }
+    if(!$refresh){
+        if(empty($cols)){
+            return $data_global[$table][$id];
+        }
+        else{
+            $data_got = [];
+            foreach($cols as $col){
+                $data_got[$col] = $data_global[$table][$id][$col];
+            }
+            return $data_got;
+        }
+    }
+    if($refresh){
+        $data_got = [];
+        if(empty($cols)){
+            $scandir = scandir("data/$table/");
+            foreach($scandir as $item){
+                if($item == "." || $item == "..") continue;
+                $cols[] = $item;
+            }
+        }
+        foreach($cols as $col){
+            $value = "";
+            $value_file = "data/$table/$col/$id";
+            if(file_exists($value_file)){
+                $value = file_get_contents($value_file);
+            }
+            $data_got[$col] = $value;
+        }
+        $data_global[$table][$id] = $data_got;
+        return $data_got;
+    }
+    die("eh?");
 }
