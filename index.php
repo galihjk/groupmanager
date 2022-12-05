9acb0e86059f65fb5ff0f62a007ef97987f5da26
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2022-12-05 13:25:18 +0700

    cobalagi

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php echo "hello world"; //--tes
+$folder_data = "/data/tes/";
+if(!file_exists($folder_data)){
+    mkdir($folder_data, 0777);
+    echo "DIBUAT: [$folder_data] ";
+}
 $folder = "/data/tes/nama/";
 if(file_exists($folder)){
     print_r(scandir($folder));
