fad16d2aa3d49dcf550548906ad0e15a9a28ad86
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2022-12-05 13:21:48 +0700

    coba

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php echo "hello world"; //--tes
-$fi = new FilesystemIterator('data/tes/nama', FilesystemIterator::SKIP_DOTS);
-printf("There were %d Files", iterator_count($fi));
+$folder = "/data/tes/nama/";
+if(file_exists($folder)){
+    print_r(scandir($folder));
+}
+else{
+    mkdir($folder, 0777);
+    echo "DIBUAT";
+}
