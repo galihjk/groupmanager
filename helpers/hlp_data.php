917f4fbefa80b7293fd4aaac3407291890a2137d
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2022-12-05 13:47:56 +0700

    ok

--- helpers/hlp_data.php
+++ helpers/hlp_data.php
@@ -1,4 +1,8 @@
 <?php
+function is_dir_empty($dir) {
+    if (!is_readable($dir)) return null; 
+    return (count(scandir($dir)) == 2);
+}
 
 function data_insert ($table, $data){
     // $cols = array_keys($data);
--- index.php
+++ index.php
@@ -1,8 +1,24 @@
 <?php echo "hello world"; //--tes
 
 $folder = "data/tes/nama/";
+
+include_once("helpers/hlp_data.php");
+
 if(file_exists($folder)){
-    print_r(scandir($folder));
+    if(!is_readable($folder)){
+        $id = 1;
+    }
+    else{
+        $scandir = scandir($folder);
+        $scandircount = count($scandir);
+        if($scandircount > 2){
+            $id = $scandir[$scandircount] + 1;
+        }
+        else{
+            $id = 1;
+        }
+    }
+    echo "NEW ID=$id";
 }
 else{
     mkdir($folder, 0777, true);
