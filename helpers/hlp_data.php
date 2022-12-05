70967e2cd5d62e1d3db756706286027d0949abae
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2022-12-06 05:26:39 +0700

    fixsert

--- helpers/hlp_data.php
+++ helpers/hlp_data.php
@@ -1,17 +1,23 @@
 <?php
 
-function data_insert($table, $data){
-    $cols = array_keys($data);
-    foreach($cols as $col){
+function data_insert($table, $data_insert){
+    $folder = "";
+    $checkcols = array_keys($data_insert);
+    foreach($checkcols as $col){
         $folder = "data/$table/$col/";
         if(!file_exists($folder)){
             mkdir($folder, 0777, true);
         }
     }
     $id = 1;
-    $scandir = scandir("data/$table/");
-    $foldercheck = $scandir[2] ?? false;
-    if($foldercheck) $folder = "data/$table/$foldercheck/";
+    $cols = scandir("data/$table/");
+    foreach($cols as $col) {
+        if(!isset($data_insert[$col])){
+            $data_insert[$col] = "";
+        }
+    }
+    $checkcols = $scandir[2] ?? false;
+    if($checkcols) $folder = "data/$table/$foldercheck/";
     if(is_readable($folder)){
         $scandir = scandir($folder);
         sort($scandir);
@@ -23,7 +29,7 @@ function data_insert($table, $data){
             $id = 1;
         }
     }
-    foreach($data as $col=>$val){
+    foreach($data_insert as $col=>$val){
         file_put_contents("data/$table/$col/$id",$val);
     }
     return $id;
